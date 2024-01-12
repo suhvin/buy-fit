9d842d366b5782846099fe5ffbb58ba2bdb47683
devgiljong <calf-love@naver.com>
2024-01-12 12:09:39 +0900

    :sparkles: Test

---
+++ __tests__/pattern.test.ts
@@ -0,0 +1,5 @@
+describe("dsa", () => {
+  it("", () => {
+    expect(true).toBe(true);
+  });
+});
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,37 @@
-import type { Config } from "tailwindcss"
-
+import type { Config } from "tailwindcss";
+const colors = {
+  neutral100: "#2C2932",
+  neutral80: "hsla(257, 4%, 34%, 1)",
+  neutral60: "rgba(44, 41, 50, 0.60);",
+  neutral30: "rgba(44, 41, 50, 0.30);",
+  neutral20: "rgba(44, 41, 50, 0.20);",
+  neutral10: "rgba(44, 41, 50, 0.10);",
+  gray100: "#455268",
+  gray80: "#718096",
+  gray60: "#91A3BD",
+  gray30: "#C9D8E9",
+  gray20: "#DAE6F3",
+  gray10: "#F4F9FF",
+  primary100: "#8732F4",
+  primary80: "#8A5BEF",
+  primary60: "#AC8CF1",
+  primary30: "#C0B7F5",
+  primary20: "#DADAFF",
+  primary10: "#E9E9FF",
+  negative100: "#FF6C58",
+  negative50: "#718096",
+  negative10: "#FFF0EE",
+  positive100: "#3366FF",
+  positive50: "#99B2FF",
+  positive10: "#EBF0FF",
+  gradientleft: "hsla(338, 99%, 64%, 1)",
+  gradientright: "hsla(266, 90%, 58%, 1)",
+  white: "#FFFFFF",
+  black: "hsla(0, 0%, 0%, 1)",
+};
 const config = {
   darkMode: ["class"],
-  content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
-	],
+  content: ["./pages/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}", "./app/**/*.{ts,tsx}", "./src/**/*.{ts,tsx}"],
   prefix: "",
   theme: {
     container: {
@@ -57,6 +81,8 @@ const config = {
         lg: "var(--radius)",
         md: "calc(var(--radius) - 2px)",
         sm: "calc(var(--radius) - 4px)",
+        xl: "60px",
+        full: "999px",
       },
       keyframes: {
         "accordion-down": {
@@ -75,6 +101,6 @@ const config = {
     },
   },
   plugins: [require("tailwindcss-animate")],
-} satisfies Config
+} satisfies Config;
 
-export default config
+export default config;
