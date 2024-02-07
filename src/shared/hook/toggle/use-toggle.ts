fa3b3959b865b33ac6cc3fea42f4b7eb1034db6b
devgiljong <calf-love@naver.com>
2024-02-07 11:05:49 +0900

    :sparkles: Toggle

---
+++ src/shared/hook/toggle/use-toggle.ts
@@ -0,0 +1,15 @@
+"use client";
+import React from "react";
+export const useToggle = () => {
+  const [isOpen, setOpen] = React.useState(false);
+  const onClose = () => {
+    setOpen(false);
+  };
+  const onOpen = () => {
+    setOpen(true);
+  };
+  const onOpenChange = (bool: boolean) => {
+    setOpen(bool);
+  };
+  return { isOpen, onClose, onOpen, onOpenChange };
+};
