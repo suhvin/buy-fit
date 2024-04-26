d67db1097322de745acf6f2d5da9caf42d08d7e2
devgiljong <calf-love@naver.com>
2024-04-26 18:05:53 +0900

    :construction: Logger

--- src/app/initialize/provider.tsx
+++ src/app/initialize/provider.tsx
@@ -6,8 +6,6 @@ import { Provider as ReduxProvider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import { Toaster } from "@/src/shared/common-ui/toast/toaster";
 import { TagProvider } from "@/src/shared/common-lib/tag/tag.provider";
-import LoggerContainer from "@/src/shared/common-lib/logging/logger.container";
-import { LoggerProvider } from "@/src/shared/common-lib/logging/core";
 import { ToastContextProvider } from "@/src/shared/common-ui/toast/use-toast";
 export const Provider = ({ children }: React.PropsWithChildren) => {
   return (
@@ -19,8 +17,6 @@ export const Provider = ({ children }: React.PropsWithChildren) => {
               <div className="  w-full h-full min-h-screen flex items-center justify-center bg-[#F5F5F5]">
                 <div className=" relative w-full min-h-screen h-full bg-white xs:w-[450px]">{children}</div>
               </div>
-              <LoggerContainer />
-              <LoggerProvider />
               <Toaster />
             </ToastContextProvider>
           </TagProvider>
--- src/shared/package/logger/core.ts
+++ src/shared/package/logger/core.ts
@@ -4,7 +4,7 @@ export type LogEventCreator<
   At extends string,
   Target extends string,
   Action extends string,
-  AnotherObj extends Record<string, any> & { type: string },
+  AnotherObj extends Record<string, unknown>,
   Glue extends string = "_",
 > = {
   feature: Feature;
@@ -18,17 +18,11 @@ export type LogEventCreator<
   eventNameTuple: readonly [Feature, Target, Action];
   eventPath: `${Feature}${Glue}${Page}${Glue}${At}${Glue}${Target}`;
   eventPathTuple: readonly [Feature, Page, At, Target];
-  props: {
-    eventName: readonly [Feature, Target, Action];
-    eventPath: readonly [Feature, Page, At, Target];
-  };
+  props: { eventTuple: [Feature, Page, At, Target, Action] };
   logEvent: {
     eventName: `${Feature}${Glue}${Target}${Glue}${Action}`;
     eventPath: `${Feature}${Glue}${Page}${Glue}${At}${Glue}${Target}`;
   } & AnotherObj;
-  logEventParam: {
-    eventName: readonly [Feature, Target, Action];
-    eventPath: readonly [Feature, Page, At, Target];
-  } & AnotherObj;
+  logEventParam: { eventTuple: [Feature, Page, At, Target, Action] } & AnotherObj;
 };
 export type DefaultLogEventType = LogEventCreator<string, string, string, string, string, { type: string }, "_">;
