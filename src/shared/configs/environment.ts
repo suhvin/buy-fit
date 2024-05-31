93813834ef0e5c66387185f5862d9995bbaa1ad8
devgiljong <calf-love@naver.com>
2024-05-31 12:09:53 +0900

    :construction: Add

--- src/shared/configs/environment.ts
+++ src/shared/configs/environment.ts
@@ -1,17 +1,17 @@
 // env에 형식에 맞는 환경변수 생성
 export const ENVIRONMENT_GOOGLE = {
-  gaId: process.env.GA_ID ?? "",
-  gtmId: process.env.GTM_ID ?? "",
+  GA_ID: process.env.GA_ID ?? "",
+  GTM_ID: process.env.GTM_ID ?? "",
 } as const;
 
 export const ENVIRONMENT_FIREBASE = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
-  measurementId: process.env.MEASUREMENT_ID,
+  API_KEY: process.env.FIREBASE_API_KEY,
+  AUTH_DOMAIN: process.env.FIREBASE_AUTH_DOMIN,
+  PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
+  STORAGE_BUCKET: process.env.FIREBASE_STORAGE_BUKET,
+  MESSAGING_SENDER_ID: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  APP_ID: process.env.FIREBASE_APP_ID,
+  MEASUREMENT_ID: process.env.MEASUREMENT_ID,
 };
 
 export const ENVIRONMENT = {
