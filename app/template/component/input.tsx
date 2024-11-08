f2b85d01155f0cf1102fd04157119f0c3e660e7c
suhvin <89975339+suhvin@users.noreply.github.com>
2024-11-08 15:21:49 +0900

    빌드 에러 해결

--- app/template/component/input.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-interface InputProps extends React.ComponentPropsWithoutRef<'input'> {}
-
-const Input = ({ ...attributes }: InputProps, ref: React.Ref<HTMLInputElement>) => {
-  return <input {...attributes} className=" w-[200px] h-[40px] bg-black text-white" ref={ref} />;
-};
-
-export default React.forwardRef(Input);
