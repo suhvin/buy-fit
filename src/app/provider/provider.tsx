6869f76260c1fa856c382bacc2b7c102b3f187de
suhvin <89975339+suhvin@users.noreply.github.com>
2024-11-08 16:18:29 +0900

    서스펜스  에러 해결

--- src/app/provider/provider.tsx
+++ src/app/provider/provider.tsx
@@ -1,19 +1,22 @@
-'use client';
-import type React from 'react';
-import { TanstackProvider } from './tanstak-query';
-import { Provider as JotaiProvider } from 'jotai';
-import PageLoggerProvider from './page-logger-provider';
-import { TagProvider } from './tag-provider';
+"use client";
+import type React from "react";
+import { TanstackProvider } from "./tanstak-query";
+import { Provider as JotaiProvider } from "jotai";
+import PageLoggerProvider from "./page-logger-provider";
+import { TagProvider } from "./tag-provider";
+import { Suspense } from "react";
 
 export const Provider = ({ children }: React.PropsWithChildren) => {
   return (
     <TanstackProvider>
       <JotaiProvider>
-        <div className="  w-full h-full min-h-screen flex items-center justify-center bg-[#F5F5F5]">
-          <div className=" relative w-full min-h-screen h-full bg-white xs:w-[450px]">{children}</div>
-        </div>
-        <PageLoggerProvider />
-        <TagProvider />
+        <Suspense>
+          <div className="  w-full h-full min-h-screen flex items-center justify-center bg-[#F5F5F5]">
+            <div className=" relative w-full min-h-screen h-full bg-white xs:w-[450px]">{children}</div>
+          </div>
+          <PageLoggerProvider />
+          <TagProvider />
+        </Suspense>
       </JotaiProvider>
     </TanstackProvider>
   );
--- src/app/provider/tag-provider.tsx
+++ src/app/provider/tag-provider.tsx
@@ -1,16 +1,16 @@
-'use client';
-import { useUtmGlobalAtom } from '@/src/shared/store/utm.store';
-import { useSearchParams } from 'next/navigation';
-import { useEffect } from 'react';
+"use client";
+import { useUtmGlobalAtom } from "@/src/shared/store/utm.store";
+import { useSearchParams } from "next/navigation";
+import { useEffect } from "react";
 
 export const TagProvider = () => {
   const searchParams = useSearchParams();
 
-  const utm_source = searchParams.get('utm_source');
-  const utm_medium = searchParams.get('utm_medium');
-  const utm_campaign = searchParams.get('utm_campaign');
-  const utm_content = searchParams.get('utm_content');
-  const utm_term = searchParams.get('utm_term');
+  const utm_source = searchParams.get("utm_source");
+  const utm_medium = searchParams.get("utm_medium");
+  const utm_campaign = searchParams.get("utm_campaign");
+  const utm_content = searchParams.get("utm_content");
+  const utm_term = searchParams.get("utm_term");
 
   const isCampaign = utm_source || utm_medium || utm_campaign || utm_content || utm_term;
 
@@ -19,11 +19,11 @@ export const TagProvider = () => {
   useEffect(() => {
     if (isCampaign) {
       setUtmGlobalAtom({
-        utmSource: utm_source ?? '',
-        utmMedium: utm_medium ?? '',
-        utmCampaign: utm_campaign ?? '',
-        utmContent: utm_content ?? '',
-        utmTerm: utm_term ?? '',
+        utmSource: utm_source ?? "",
+        utmMedium: utm_medium ?? "",
+        utmCampaign: utm_campaign ?? "",
+        utmContent: utm_content ?? "",
+        utmTerm: utm_term ?? "",
       });
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
